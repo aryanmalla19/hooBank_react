02b6cb2f6968e68549519424567c0c6d7bd43f0a
Aryan Malla <aryanmalla19@gmail.com>
2024-01-07 12:06:02 +0545

    Fully completed !

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,12 +5,19 @@ import FeedbackCard from "./FeedbackCard"
 function Testimonials() {
   return (
     <section id='clients' className={`${styles.paddingY} flex-col relative`}>
-        <div/>
+        <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue-gradient'/>
         <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
           <h1 className={styles.heading2}>What people are <br className='sm:block hidden' />saying about us</h1>
           <div className='w-full md:mt-0 mt-6'>
             <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
           </div>
+        </div>
+        <div className='flex flex-wrap justify-start lg:justify-center relative z-[1]'>
+          {
+            feedback.map((card)=>(
+              <FeedbackCard key={card.id} {...card} />
+            ))
+          }
         </div>
     </section>
   )
